f298e5c9fae54eaaaed9af7bc826c1f609c579d3
OmarGuajardo <omar.guajardog@outlook.com>
2019-07-24 18:38:23 -0500

    incorporated for loop to repeat li statement

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
+nameArray = ['Omar Guajardo','Luis Acosta','Omar Garza','Anahi Cantu','Denielle Islas','Jennifer Guajardo']
+
 @app.route('/')
 def login():
     return render_template('login.html')
 
 @app.route('/base')
 def base():
-    return render_template('trial.html')
+    return render_template('trial.html',nameArray = nameArray)
 
 if __name__ == "__main__":
     app.run(debug=True)
