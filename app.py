e98794c09735d7a60a3121a1e4ace9862b17a852
OmarGuajardo <omar.guajardog@outlook.com>
2019-07-29 20:11:16 -0500

    testint to make sure mergin succeded

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ import requests
 app = Flask(__name__)
 Bootstrap(app)
 
-trial = True 
+
 
 nameArray = ['Omar Guajardo','Luis Acosta','Omar Garza','Anahi Cantu','Denielle Islas','Jennifer Guajardo']
 
 
 @app.route('/')
 def login():
-    return render_template('login.html',trial = trial)
+    return render_template('login.html')
 
 @app.route('/manage')
 def base():
     return render_template('index.html',
     nameArray = nameArray
-    
     )
 
 if __name__ == "__main__":
